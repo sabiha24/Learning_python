373182a19ec2613dee25492fe964d330daebbb5f
sabiha24 <pattansabiha24@gmail.com>
2025-05-30 20:59:00 +0530

    Create day_1.py

---
+++ day_1.py
@@ -0,0 +1,12 @@
+#Basics
+print("Hello World")
+
+#concatenation
+print("Hello"+" "+"Sabiha")
+
+#length of a given string
+username=(input("What is your name?"))
+length=len(username)
+print(length)
+
+
