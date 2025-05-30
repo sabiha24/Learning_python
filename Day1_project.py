cb06c8909746b47480733149a72e1d3f8ad18a5f
sabiha24 <pattansabiha24@gmail.com>
2025-05-30 21:01:17 +0530

    Create Day1_project.py

---
+++ Day1_project.py
@@ -0,0 +1,5 @@
+#band name Genarator project
+print("Welcome to the Band Name Generator")
+city=input("what is the city u grew up in?\n")
+pet=input("what is your pet's name?\n")
+print("Your Band name could be "+city+" "+pet)
