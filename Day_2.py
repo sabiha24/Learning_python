7a3d3e4fb8f5ae84092d75f8ae599c9c11f9e623
sabiha24 <pattansabiha24@gmail.com>
2025-05-30 21:08:51 +0530

    Create Day_2.py

---
+++ Day_2.py
@@ -0,0 +1,10 @@
+#Data types
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3)
+print(2//3)
+print(2**3)
+#type casting
+print(int("123")+int("321"))
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
